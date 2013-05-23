e6b77aca555ffc9f51bce893898aa01874407f1b
Rouan Wilsenach <rouanw@gmail.com>
2013-05-23 23:03:43 +0300

    added test project

---
+++ MvcApplication.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MvcApplication.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(2, 1+1);
+        }
+    }
+}
